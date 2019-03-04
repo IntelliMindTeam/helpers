3e0d9b2089331e40d846cbebd843e334cc75807a
Rajat Movaliya <rajat@intellimind.io>
2019-03-04 15:47:31 +0530

    adding support for aws s3 files downloading by year and date-range

--- helpers/aws/s3.py
+++ helpers/aws/s3.py
@@ -4,8 +4,16 @@ import os
 import sys
 import shutil
 import datetime
+import uuid
 
-def upload_to_s3(bucket_name, local_source_path, remote_target_path, is_dir=False):
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from log import get_logger
+
+logger = get_logger('s3')
+
+def upload_to_s3(bucket_name, local_source_path, remote_target_path,\
+	is_dir=False):
 	''' Uploading given file or dir to s3 bucket'
 
 		Parameters :
@@ -64,9 +72,11 @@ def upload_to_s3(bucket_name, local_source_path, remote_target_path, is_dir=Fals
 			raise Exception('Error while uploading file - {} :: {}'.format(\
 				local_source_path, ex))
 
-def backup_to_s3(bucket_name, source_dir, dest_dir, format='%Y-%m-%d', sub_dir=None):
+def backup_to_s3(bucket_name, source_dir, dest_dir, \
+	format='%Y-%m-%d', sub_dir=None):
 	'''
-		To create zipped s3-backup of sub-directory that has name as todays's date
+		To create zipped s3-backup of sub-directory
+		that has name as todays's date
 
 		Parameters:
 
@@ -112,13 +122,144 @@ def backup_to_s3(bucket_name, source_dir, dest_dir, format='%Y-%m-%d', sub_dir=N
 	os.remove(local_source_path)
 
 
+def get_matching_s3_keys(bucket, prefix='', suffix=''):
+	"""
+	Generate the keys in an S3 bucket.
 
-def download_from_s3(bucket_name, start_date, end_date):
+	Parameters:
+	bucket: Name of the S3 bucket.
+	prefix: Only fetch keys that start with this prefix (optional).
+	suffix: Only fetch keys that end with this suffix (optional).
+	"""
+
+	s3_client = boto3.client('s3')
+
+	kwargs = {'Bucket': bucket, 'Prefix': prefix}
+
+	while True:
+		resp = s3_client.list_objects_v2(**kwargs)
+		for obj in resp['Contents']:
+			key = obj['Key']
+			if key.endswith(suffix):
+				yield key
+
+		try:
+			kwargs['ContinuationToken'] = resp['NextContinuationToken']
+		except KeyError:
+			break
+
+def get_file_paths(remote_source_dir, start_date, end_date,\
+	date_format, suffix):
+	''' It will yield final file paths '''
+
+	# extracting all dates between given range
+	start_date = datetime.datetime.strptime(start_date, date_format).date()
+	end_date = datetime.datetime.strptime(end_date, date_format).date()
+	days_delta = (end_date - start_date).days
+
+	# creating final remote file_path(key)
+	for days in range(0, days_delta + 1):
+
+		date = start_date + datetime.timedelta(days=days)
+		file_name = date.strftime(date_format) + suffix
+
+		yield os.path.join(
+			remote_source_dir,
+			str(date.year),
+			str(date.month),
+			file_name,
+		)
+
+def download_files_by_date_range(bucket_name, remote_source_dir, \
+	local_target_dir, start_date, end_date, \
+	date_format='%Y-%m-%d', suffix='.tar.gz'):
 	''' It will download files from s3 bucket
 		by filtering with date-range
-		format : YYYY-mm-dd
+
+		Note :
+		* month is assumed to be name as in its integer form
+		e.g  for MARCH month directory name should be '3' not 03'
+
+		Params:
+		bucket_name :		name of the s3 bucket
+		remote_source_dir :	from which dir to find files in s3
+		local_target_dir :	local dir path to put downloaded files
+		start_date : 		start-date (inclusive)
+		end_date : 			end-date (inclusive)
+		date_format :		format of file_name convention
+							and of string date parameters
 	'''
 
-	base = datetime.datetime.strptime(start_date)
-	date_list = [base - datetime.timedelta(days=x) for x in range(0, numdays)]
+	s3_resouce = boto3.resource('s3')
+	bucket = s3_resouce.Bucket(bucket_name)
 
+	kargs = {
+		'remote_source_dir': remote_source_dir,
+		'start_date': start_date,
+		'end_date': end_date,
+		'date_format': date_format,
+		'suffix': suffix
+	}
+
+	for key in get_file_paths(**kargs):
+
+		local_file_path = os.path.join(local_target_dir,\
+			os.path.basename(key))
+
+		# download only if file exists else continue
+		try:
+			logger.info('Checking for key : %s' % str(key))
+			bucket.download_file(key, local_file_path)
+		except:
+			continue
+
+def download_files_by_year(bucket_name, year, remote_source_dir,\
+	local_target_dir, suffix='.tar.gz'):
+	'''
+		download all files in given year dir from s3
+
+		Parametes:
+		bucket_name : Name of bucket in s3
+		year: name of year dir to be download,
+		remote_source_dir : base path of remote s3
+		local_target_dir : local base path where to download files
+	'''
+
+	s3_resouce = boto3.resource('s3')
+	bucket = s3_resouce.Bucket(bucket_name)
+	prefix = os.path.join(remote_source_dir, str(year))
+
+	for key in get_matching_s3_keys(bucket_name, prefix=prefix,\
+		suffix=suffix):
+
+		local_file_path = os.path.join(local_target_dir, \
+			os.path.basename(key))
+
+		# download only if file exists else continue
+		try:
+			logger.info('Processing key : %s' % str(key))
+			bucket.download_file(key, local_file_path)
+		except Exception as ex:
+			logger.error(ex)
+			continue
+
+def create_bucket_name(bucket_prefix):
+	''' returns unique bucket_name '''
+
+    # The bucket name must be between 3 and 63 chars long
+	return bucket_prefix + str(uuid.uuid4())
+
+
+def create_bucket(bucket_prefix, s3_connection):
+	''' creating new bucket '''
+
+	session = boto3.session.Session()
+	current_region = session.region_name
+	bucket_name = create_bucket_name(bucket_prefix)
+
+	bucket_response = s3_connection.create_bucket(
+		Bucket=bucket_name,
+		CreateBucketConfiguration={
+		'LocationConstraint': current_region})
+
+	return bucket_name, bucket_response
