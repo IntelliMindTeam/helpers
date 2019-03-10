181e536dba066b5b75d77ea7a68f0a6b4a004b52
Rajat Movaliya <rajat@intellimind.io>
2019-03-10 14:09:02 +0530

    adding redis in connections

--- helpers/connections/__init__.py
+++ helpers/connections/__init__.py
@@ -1,6 +1,7 @@
 import arctic
 import pymysql
 import rethinkdb as r
+import redis
 
 def get_arctic_store(config):
 	''' get arctic connection '''
@@ -15,13 +16,21 @@ def get_mysql_connection(config):
                             passwd=config.get('MYSQL', 'MYSQL_PASSWD'),
                             db=config.get('MYSQL', 'MYSQL_DATABASE'))
 
-def get_rethink_connection(props):
+def get_rethink_connection(config):
 	''' get rethink db connection  '''
 
-	rethink_conn = r.connect(host=props.get('RETHINKDB', 'RETHINK_HOST'),\
-								port=props.get('RETHINKDB', 'RETHINK_PORT'),\
-								db=props.get('RETHINKDB', 'RETHINK_DB'),\
-								user=props.get('RETHINKDB', 'RETHINK_USER'),\
-								password=props.get('RETHINKDB', 'RETHINK_PASSWORD'),\
-								timeout=int(props.get('RETHINKDB', 'RETHINK_TIMEOUT')))
+	rethink_conn = r.connect(host=config.get('RETHINKDB', 'RETHINK_HOST'),\
+								port=config.get('RETHINKDB', 'RETHINK_PORT'),\
+								db=config.get('RETHINKDB', 'RETHINK_DB'),\
+								user=config.get('RETHINKDB', 'RETHINK_USER'),\
+								password=config.get('RETHINKDB', 'RETHINK_PASSWORD'),\
+								timeout=int(config.get('RETHINKDB', 'RETHINK_TIMEOUT')))
 	return rethink_conn
+
+def get_redis_connection(config):
+
+	return redis.Redis(
+		host=config.get('REDIS', 'REDIS_HOST'),\
+		port=config.get('REDIS', 'REDIS_PORT'),\
+		db=config.get('REDIS', 'REDIS_DB'),\
+	)
