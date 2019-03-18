8d2847da775e6b436810d5dafc63779d71e30701
Rajat Movaliya <rajat@intellimind.io>
2019-03-18 17:51:54 +0530

    adding e.g for exception decorator

--- helpers/exception/__init__.py
+++ helpers/exception/__init__.py
@@ -3,14 +3,28 @@ from helpers.log import get_logger
 
 logger = get_logger('exception')
 
-#......... Simple Generic Exception Handler Decorator .........#
+#......... Simple Decorator for Generic Exception Handler .........#
 
 def exception_handler(return_if_exception=None):
 	''' decorator for generic exeption handling
 
 		Params:
 		* return_if_exception : it will be returned if any
-								exeption occurs
+								exeption occurs o/w normal return
+		e.g.
+		----------------------------------------------------------
+		# return empty list if any exception occur o/w normal flow
+
+		@exception_handler(return_if_exception=[])
+		def my_func(a, b):
+			...
+			...
+		----------------------------------------------------------
+		Note: If any exception occur during function run, it will be handled by
+		this decorator, logged appropriately and
+		return value as you specified in argument, (only if any exception)
+		default will be None
+
 	'''
 
 	def real_decorator(method):
