d0d283988fd7110e4869850493fc421930e47cc8
Rajat Movaliya <rajat@intellimind.io>
2019-03-18 11:52:03 +0530

    adding generic exception handler

---
+++ helpers/exception/__init__.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from helpers.log import get_logger
+
+logger = get_logger('exception')
+
+#......... Simple Generic Exception Handler Decorator .........#
+
+def exception_handler(return_if_exception=None):
+	''' decorator for generic exeption handling
+
+		Params:
+		* return_if_exception : it will be returned if any
+								exeption occurs
+	'''
+
+	def real_decorator(method):
+
+		@wraps(method)
+		def wrapper(*args, **kargs):
+
+			try:
+
+				response = method(*args, **kargs)
+
+			except Exception as ex:
+				response = return_if_exception
+				logger.error('Error in {} , args : {} {} :: {}'.format(\
+					method.__name__, args, kargs, str(ex)))
+			finally:
+				return response
+
+		return wrapper
+
+	return real_decorator
